c4f494fe1f3cc86ddece2a13a534306f6172c634
Ruslan Lozhkin <18569837+RuslanLozhkin@users.noreply.github.com>
2019-11-05 15:04:17 +0200

    try to make all adaptetive for IE11 but it's not support svg sprites

--- dist/bundle.js
+++ dist/bundle.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0cf4acb90b07d056f828";
+/******/ 	var hotCurrentHash = "4a4df6cc279e9f8a7f1f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
